2aab54f38fef812a8561ad963d605f51bc8d1fae
MikeJoe789 <juntaoli2022@gmail.com>
2023-11-24 10:25:16 +0000

    add routes

---
+++ src/pages/Article.jsx
@@ -0,0 +1,7 @@
+const Article = ()=>{
+    return(
+        <h1>This is Article page!</h1>
+    )
+}
+
+export default Article;
---
+++ src/pages/ArticleList.jsx
@@ -0,0 +1,7 @@
+const ArticleList = ()=>{
+    return(
+        <h1>This is ArticleList page!</h1>
+    )
+}
+
+export default ArticleList;
